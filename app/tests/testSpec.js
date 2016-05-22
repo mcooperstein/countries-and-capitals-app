7ee52d3eeaa6d1466deb0a479e0ab70931c636d4
Marc Cooperstein <mcooperstein@gmail.com>
2016-05-22 12:14:03 -0700

    added jasmine/karma tests
    testing that country selection renders correct countryName

---
+++ app/tests/testSpec.js
@@ -0,0 +1,19 @@
+describe('countriesController', function () {
+    var controller = null;
+    $scope = null;
+
+    beforeEach(function () {
+        module('myApp');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        $scope = $rootScope.$new();
+        controller = $controller('countriesController', {
+            $scope: $scope
+        });
+    }));
+
+    it('initial predicate value should equal country name', function () {
+        expect($scope.predicate).toEqual('countryName');
+    });
+});
